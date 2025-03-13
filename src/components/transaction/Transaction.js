405554d58150e94493930e2e57889d3d7228a316
Your Name <you@example.com>
2025-03-13 05:12:19 -0700

    code

--- src/components/transaction/Transaction.js
+++ src/components/transaction/Transaction.js
@@ -15,6 +15,7 @@ import DateRangePicker from './DateRangePicker';
 import { addHours, numberWithCommas } from '../qickfun/qickfun';
 import { DateTime } from 'luxon';
 import { DataContext } from "../APIs/Api";
+import { use } from 'react';
 
 
 
@@ -66,7 +67,206 @@ const Transaction = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [activeButton, setActiveButton] = useState("transaction")
   const [dateRange, setDateRange] = useState([]);
- 
+  const [options, setOptions] = useState();
+
+  console.log({options});
+  
+  useEffect(() => {
+
+    API.getCurrency()
+    .then((result) => {
+      //console.log(result);
+      // setOptions(result.currency_options)
+
+      // console.log({options});
+
+      if(result.currency_options) {
+       setOptions(result.currency_options)
+        
+      } else {
+        setOptions({
+          'USD':{
+              'symbol':'$',
+              'name':'Dollar',
+              'code':'USD',
+              'rate':1
+          },
+          'IDR':{
+              'symbol':'Rp',
+              'name':'Indonesian Rupiah',
+              'code':'IDR',
+              'rate':16000
+          },
+          'MMK':{
+              'symbol':'K',
+              'name':'Myanmar',
+              'code':'MMK',
+              'rate':2000
+          },
+          'XOF':{
+              'symbol':'CFA',
+              'name':'African CFA franc',
+              'code':'XOF',
+              'rate':600
+          },
+          'PKR':{
+              'symbol':'Rs',
+              'name':'Pakistani Rupee',
+              'code':'PKR',
+              'rate':300
+          },
+          'KES':{
+              'symbol':'KES',
+              'name':'Kenyan Shilling',
+              'code':'KES',
+              'rate':130
+          },
+          'NGN':{
+              'symbol':'₦',
+              'name':'Nigeria Naira',
+              'code':'NGN',
+              'rate':1000
+          },
+          'UGX':{
+              'symbol':'UGX',
+              'name':'Uganda Shilling',
+              'code':'UGX',
+              'rate':3000
+          },
+          'VND':{
+              'symbol':'₫',
+              'name':'Vietnamese Dong',
+              'code':'VND',
+              'rate':25000
+          },
+          'ZMW':{
+              'symbol':'ZK',
+              'name':'Zambian Kwacha',
+              'code':'ZMW',
+              'rate':26
+          },
+          'UZS':{
+              'symbol':'лв',
+              'name':'Uzbekistani Som',
+              'code':'UZS',
+              'rate':12800
+          },
+          'GHS':{
+              'symbol':'₵',
+              'name':'Ghanaian Cedi',
+              'code':'GHS',
+              'rate':15
+          },
+          'RUB':{
+              'symbol':'₽',
+              'name':'Russian Ruble',
+              'code':'RUB',
+              'rate':90
+          },
+          'BRL':{
+              'symbol':'R$',
+              'name':'Brazilian Real',
+              'code':'BRL',
+              'rate':6
+          }
+      
+      })
+      }
+      
+    })
+    .catch((err) => {
+      console.log(err);
+      setOptions({
+        'USD':{
+            'symbol':'$',
+            'name':'Dollar',
+            'code':'USD',
+            'rate':1
+        },
+        'IDR':{
+            'symbol':'Rp',
+            'name':'Indonesian Rupiah',
+            'code':'IDR',
+            'rate':16000
+        },
+        'MMK':{
+            'symbol':'K',
+            'name':'Myanmar',
+            'code':'MMK',
+            'rate':2000
+        },
+        'XOF':{
+            'symbol':'CFA',
+            'name':'African CFA franc',
+            'code':'XOF',
+            'rate':600
+        },
+        'PKR':{
+            'symbol':'Rs',
+            'name':'Pakistani Rupee',
+            'code':'PKR',
+            'rate':300
+        },
+        'KES':{
+            'symbol':'KES',
+            'name':'Kenyan Shilling',
+            'code':'KES',
+            'rate':130
+        },
+        'NGN':{
+            'symbol':'₦',
+            'name':'Nigeria Naira',
+            'code':'NGN',
+            'rate':1000
+        },
+        'UGX':{
+            'symbol':'UGX',
+            'name':'Uganda Shilling',
+            'code':'UGX',
+            'rate':3000
+        },
+        'VND':{
+            'symbol':'₫',
+            'name':'Vietnamese Dong',
+            'code':'VND',
+            'rate':25000
+        },
+        'ZMW':{
+            'symbol':'ZK',
+            'name':'Zambian Kwacha',
+            'code':'ZMW',
+            'rate':26
+        },
+        'UZS':{
+            'symbol':'лв',
+            'name':'Uzbekistani Som',
+            'code':'UZS',
+            'rate':12800
+        },
+        'GHS':{
+            'symbol':'₵',
+            'name':'Ghanaian Cedi',
+            'code':'GHS',
+            'rate':15
+        },
+        'RUB':{
+            'symbol':'₽',
+            'name':'Russian Ruble',
+            'code':'RUB',
+            'rate':90
+        },
+        'BRL':{
+            'symbol':'R$',
+            'name':'Brazilian Real',
+            'code':'BRL',
+            'rate':6
+        }
+    
+    })
+    
+    }
+    )
+  }, [])
 
   useEffect(() => {
     
@@ -123,6 +323,13 @@ const Transaction = () => {
 
   //console.log(data);
   const transactionCard = data.map(e => {
+      let cs;
+      if(options !== undefined) {
+        cs = options[e.currency].
+        symbol;
+        //console.log({cs});
+        
+      }
       // if(e.status === "success") {
       //   return (<><TransactionCard /></>)
       // } else if(e.status === "success") {
@@ -134,7 +341,7 @@ const Transaction = () => {
       // }
       return (
         <TransactionCard key={e.id}
-        id={e.sessionID} type={e.type} amount={numberWithCommas(Number(e.amount).toFixed(2))} date={e.timestamp} status={e.status} currency={e.currency}
+        id={e.sessionID} type={e.type} amount={numberWithCommas(Number(e.amount).toFixed(2))} date={e.timestamp} status={e.status} currency={cs !== undefined ? cs : ""}
         />
       )
     })
